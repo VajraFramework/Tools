08d5a0a49303a124981cb15d4624a07eacc11aff
cosmiczilch <cosmiczilch@gmail.com>
2014-01-20 17:25:13 -0500

    Generating C++ code to map components/properties names to functions.

--- ComponentParser/main.pl
+++ ComponentParser/main.pl
@@ -7,15 +7,12 @@ require "Property.pl";
 require "Component.pl";
 require "Parser.pl";
 
-my $vajraParser = Parser->new();
-$vajraParser->parseHeaderFiles("../../", "Vajra");
-# $vajraParser->debug_PrintComponents();
-$vajraParser->exportXml("../../ExampleGame/Resources/ComponentSpecifications/VajraComponents/Components.xml");
-
-my $gameParser = Parser->new();
-$gameParser->parseHeaderFiles("../../ExampleGame/Code/", "ExampleGame");
-# $gameParser->debug_PrintComponents();
-$gameParser->exportXml("../../ExampleGame/Resources/ComponentSpecifications/GameComponents/Components.xml");
+my $parser = Parser->new();
+$parser->parseHeaderFiles("../../", "Vajra");
+$parser->parseHeaderFiles("../../ExampleGame/Code/", "ExampleGame");
+# $parser->debug_PrintComponents();
+$parser->exportXml("../../ExampleGame/Resources/ComponentSpecifications/Components.xml");
+$parser->generateCplusplus("../../ExampleGame/Code/ExampleGame/ComponentMapper/ComponentMapper.cpp");
 
 printf "\n\nDONE\n";
 
